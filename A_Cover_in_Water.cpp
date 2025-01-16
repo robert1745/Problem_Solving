288e949229e27692f778070a7fd5ed81894c3949
robert1745 <0745naveen@gmail.com>
2025-01-16 22:22:02 +0530

    Today cf done!

---
+++ A_Cover_in_Water.cpp
@@ -0,0 +1,35 @@
+/* जय श्री हनुमान */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin>>s;
+    for(int i =0;i<s.length();i++){
+        if(s[i]=='.'){
+
+        }
+        if(s[i]=='#'){
+            
+        }
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test = 1;
+    cin>>test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
